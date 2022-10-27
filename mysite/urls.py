af1172ea1c274fda92775fab33330e8e10695f2d
min jeong kim <minjeong0887921@gmail.com>
2022-10-27 21:31:11 +0900

    example 코드 삭제, 기존 코드 살림

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static 
 from django.contrib import admin
+from django.urls import path, include
+
+from mysite.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    # path('', include(router.urls)),
-    path('api2/', include('api2.urls')),
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    path('api/', include('api.urls')),   # 클래스형 Vue
+    path('api2/', include('api2.urls')), # DRF 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
